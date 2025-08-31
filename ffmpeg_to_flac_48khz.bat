e502282e54d32b7ab7188c5099dce04a84cdb140
bigxho <bigxho15@gmail.com>
2025-08-31 21:05:50 +0200

    Add files via upload

---
+++ ffmpeg_to_flac_48khz.bat
@@ -0,0 +1,13 @@
+@echo on
+@echo ***Convert to flac***
+@echo ***Put this batch file in root folder***
+:inconv
+@set /P inconv="File type to convert, sample .mp4 -> "
+@dir /b *%inconv% >list.txt
+@md flac
+@cls
+@FOR /F "eol=f  tokens=1 delims=." %%p in (list.txt) DO (ffmpeg.exe -hwaccel_device 0 -i "%%p%inconv%" -af aformat=s16:48000 -compression_level 9 "flac/%%p.flac")
+@del /f list.txt >nul
+@echo __End of Converted File__
+@echo Press Enter to Exit...
+@pause >nul
